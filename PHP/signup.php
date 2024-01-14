613d9578a20c9ef2db7020687f3f511932699efe
Chadiaaaa <91328138+Chadiaaaa@users.noreply.github.com>
2024-01-14 18:59:19 +0100

    Update signup.php

--- PHP/signup.php
+++ PHP/signup.php
@@ -6,7 +6,7 @@ $mdp = $_GET['password'];
 $mail = $_GET['mail'];
 
 // Préparer et exécuter la requête SQL d'insertion
-$requete = $connexion->prepare("INSERT INTO `data` (`username`, `mdp`,`mail`) VALUES (?, ?, ?)");
+$requete = $connexion->prepare("INSERT INTO data (username, mdp, mail) VALUES (?, ?, ?)");
 $requete->bind_param("sss", $username, $mdp, $mail);
 $requete->execute();
 
