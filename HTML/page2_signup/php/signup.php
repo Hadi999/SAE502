829c2a72df18f6ec429ab32662ddc7372cd1e70f
kheiro12 <152870265+kheiro12@users.noreply.github.com>
2024-01-14 20:00:00 +0100

    Update signup.php

--- HTML/page2_signup/php/signup.php
+++ HTML/page2_signup/php/signup.php
@@ -1,6 +1,12 @@
 <?php
 include("connexion_database.php");
 
+// Validation du mot de passe 
+if (!validerMotDePasse($mdp)) {
+    echo "Le mot de passe doit contenir au moins 8 caractères, dont une majuscule et un chiffre.";
+    exit;
+}
+
 $username = $_GET['username'];
 $mdp = $_GET['password'];
 $mail = $_GET['mail'];
@@ -13,5 +19,11 @@ $requete->execute();
 // Fermer la connexion à la base de données
 $requete->close();
 $conn->close();
+
+// Fonction pour valider le mot de passe
+function validerMotDePasse($mdp) {
+    return preg_match("/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/", $mdp);
+}
+
 ?>
 
