d9280082f429e12c10fc356761a4ff9aca822311
kheiro12 <152870265+kheiro12@users.noreply.github.com>
2024-01-23 17:29:57 +0100

    Update signin.php

--- Docker2/HTML/signin.php
+++ Docker2/HTML/signin.php
@@ -70,30 +70,31 @@
 
 <?php
 include("connexion_database.php");
-
-$username = $_GET['username'];
-$mdp = $_GET['password'];
-
-
-//Vérification des données dans la base de données
-if (!empty($username) AND !empty($mdp) AND !empty($mail)){
-    $search=$conn->prepare('SELECT * FROM data WHERE username=? AND mdp=?');
-    $search->execute(array($username, $mdp));
-    $found=$search->rowCount();
-    //echo $found; 
-
-    if ($found==1){
-        header('location:Accueil.html'); //METTRE LA PAGE APRES AUTHENTIFICATION DE L'UTILISATEUR
+    
+if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['submit'])) {
+    $username = isset($_GET['username']) ? $_GET['username'] : '';
+    $mdp = isset($_GET['password']) ? $_GET['password'] : '';
+    
+    
+    //Vérification des données dans la base de données
+    if (!empty($username) AND !empty($mdp) AND !empty($mail)){
+        $search=$conn->prepare('SELECT * FROM data WHERE username=? AND mdp=?');
+        $search->execute(array($username, $mdp));
+        $found=$search->rowCount();
+        //echo $found; 
+    
+        if ($found==1){
+            header('location:Accueil.html'); //METTRE LA PAGE APRES AUTHENTIFICATION DE L'UTILISATEUR
+        }
+        else{
+        echo "Des informations ne sont pas correct"; //Peut etre mit sous forme de pop up sur la page HTML
+        }
     }
     else{
-    echo "Des informations ne sont pas correct"; //Peut etre mit sous forme de pop up sur la page HTML
+        echo "Les champs ne sont pas complet";
     }
+    $search->close();
+    $found->close();
+    $conn->close();
 }
-else{
-    echo "Les champs ne sont pas complet";
-}
-$search->close();
-$found->close();
-$conn->close();
-
 ?>
